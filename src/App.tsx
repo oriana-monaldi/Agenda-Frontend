61b06f51030ed3107fdb4d87ae6b60100a2f2d63
oriana-monaldi <orimonaldi@gmail.com>
2025-03-21 15:29:38 -0300

    frontend

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+/* import  {Calendar} from 'react-big-calendar' */
+
+function App() {
+
+  return (
+    <div className='bg-red-300'>
+      <div >
+        <h1 className='text-2xl text-center text-black p-2'>Claudia Echavarry Estilista</h1>
+      </div>
+    <div>
+{/*       <Calendar />
+ */}    </div>
+    </div>
+  )
+}
+
+export default App
