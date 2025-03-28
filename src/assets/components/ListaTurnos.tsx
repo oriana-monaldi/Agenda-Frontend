0ed8ce160054b1c05b6c39eab9e98bd897b22051
oriana-monaldi <orimonaldi@gmail.com>
2025-03-28 12:39:11 -0300

    misc

--- src/assets/components/ListaTurnos.tsx
+++ src/assets/components/ListaTurnos.tsx
@@ -1,13 +1,14 @@
 import { IoAddCircleOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import Calendario from "./Calendario";
+
 function ListaTurnos() {
   return (
     <div>
-      <div className="flex items-center justify-between mb-6">
+      <div className="flex items-center justify-center mb-4">
         <Link
           to="/nuevo-turno"
-          className=" text-black px-4 py-2 rounded-lg flex items-center gap-2"
+          className="text-black px-4 py-2 rounded-lg flex items-center gap-2 hover:bg-gray-100 transition-colors"
         >
           <IoAddCircleOutline className="text-xl" color="red" />
           CARGAR UN NUEVO TURNO
